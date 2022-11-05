3b526267b73bd64dcfb780c10759c6608902eb4b
Aslan <45170182+CodeForse@users.noreply.github.com>
2022-11-06 01:27:55 +0600

    added adding to the table

--- postgress/connecting_to_db.py
+++ postgress/connecting_to_db.py
@@ -8,5 +8,12 @@ keys=Env_keys(_env_file='.env',_env_file_encoding='utf-8')
 
 connect=psycopg2.connect(database='First Test',user='postgres',host='localhost',password=keys.db_pass)
 cursor=connect.cursor()
-cursor.execute('select * from Notifications where id=1')
+cursor.execute('select * from Notifications where id>0')
 print(cursor.fetchall())
+
+def add_new_notif(id:int,mess:str,date:str):
+    cursor.execute(
+        "insert into Notifications values({}, '{}', '{}')".format(str(id),mess,date))
+    connect.commit()
+
+
